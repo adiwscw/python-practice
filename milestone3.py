26df4e77f073ca1ccb8599c7bc99e29bd18d5398
adiwscw <talwara@wsc.school.nz>
2025-05-27 12:27:20 +1200

    Update milestone3.py

--- milestone3.py
+++ milestone3.py
@@ -1,2 +1,13 @@
-start_or_no = input("Hello, welcome to this quiz. Do you wish to play? y or n: ")
-
+start_or_no = input("Hello, welcome to this quiz. Do you wish to play? y or n: ").lower()
+points = 0
+if start_or_no == "n":
+    exit
+elif start_or_no == "y":
+    question_1 = input("What is 2+2: ")
+    if question_1 == 4:
+      points += 1
+      question_2 = input("What is 4 x 4: ")
+    elif question_2 == 16:
+      
+else:
+    print("Invalid Text")
